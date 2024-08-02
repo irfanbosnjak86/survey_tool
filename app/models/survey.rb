73abf50088bf39515152246b9afbca9cf2215df2
Irfan Bosnjak <irfan.bosnjak86@gmail.com>
2024-08-02 18:16:13 +0200

    Add validations for survey and test cases

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,2 +1,3 @@
 class Survey < ApplicationRecord
+  validates_presence_of :title, :question
 end
--- test/models/survey_test.rb
+++ test/models/survey_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class SurveyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  class Validation < ActiveSupport::TestCase
+    test "should not save survey without title" do
+      survey = Survey.new(question: "test question?")
+
+      assert_not survey.save
+    end
+
+    test "should not save survey without question" do
+      survey = Survey.new(title: "test title")
+
+      assert_not survey.save
+    end
+
+    test "should save survey with valid attributes" do
+      survey = Survey.new(title: "test title", question: "test question?")
+
+      assert survey.save
+    end
+  end
 end
